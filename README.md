e2567924ad71291c061a7d08773de1793d7913cd
Jieun Rim <rim.jieun@gmail.com>
2017-06-14 22:31:35 -0400

    adding portfolio files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Basic-Portfolio
+# portfolio
+my portfolio
---
+++ public/assets/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  $("#intro-btn, .side-nav#intro-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section2").offset().top},
+        'slow');
+  });
+
+  $("#skills-btn, .side-nav#intro-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section3").offset().top},
+        'slow');
+  });
+
+  $("#portfolio-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section4").offset().top},
+        'slow');
+  });
+
+  $("#contact-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section5").offset().top},
+        'slow');
+  });
+});
