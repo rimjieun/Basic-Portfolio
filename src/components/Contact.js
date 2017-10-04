e57e68b2ece2177c57a382289fac878918f0c512
Jieun Rim <rim.jieun@gmail.com>
2017-10-04 02:56:24 -0400

    fixing textarea width on contacts page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => (
       </div>
       <div className='flex-row form-item'>
         <label htmlFor='message'><i className="fa fa-pencil" aria-hidden="true"></i></label>
-        <textarea rows='6' cols='56' name='message' id='message' className='form-item' placeholder='Message'></textarea>
+        <textarea rows='6' name='message' id='message' className='form-item' placeholder='Message'></textarea>
       </div>
       <input type='submit' id='submit' value='S E N D' onSubmit={e => e.preventDefault()}/>
     </form>
