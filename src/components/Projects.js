05ff3a781a7e07dae75b599ebd99ad45f0406dae
Jieun Rim <rim.jieun@gmail.com>
2017-10-05 04:27:02 -0400

    updated project item

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,25 +10,43 @@ class Projects extends Component {
           id: 1,
           title: 'MeetPoint',
           imgURL: './assets/img/meetpoint.jpg',
-          color: 'rgba(255, 165, 0, 0.7)'
+          color: 'rgba(255, 165, 0, 0.6)',
+          ongoing: false
         },
         {
           id: 2,
           title: 'SignatureFD',
           imgURL: './assets/img/signaturefd.jpeg',
-          color: 'rgba(0, 0, 153, 0.7)'
+          color: 'rgba(0, 0, 0, 0.5)',
+          ongoing: false
         },
         {
           id: 3,
           title: 'Smart3DATL',
           imgURL: './assets/img/marta.jpg',
-          color: 'rgba(0, 102, 0, 0.7)'
+          color: 'rgba(0, 102, 0, 0.6)',
+          ongoing: false
         },
         {
           id: 4,
           title: 'Braves Concessions',
           imgURL: './assets/img/braves.jpeg',
-          color: 'rgba(153, 0, 0, 0.7)'
+          color: 'rgba(153, 0, 0, 0.6)',
+          ongoing: false
+        },
+        {
+          id: 5,
+          title: 'BootCruit',
+          imgURL: './assets/img/bootcruit.jpg',
+          color: 'rgba(0, 34, 102, 0.6)',
+          ongoing: true
+        },
+        {
+          id: 6,
+          title: 'Roster Scraper',
+          imgURL: './assets/img/bail.jpeg',
+          color: 'rgba(102, 0, 102, 0.6)',
+          ongoing: true
         }
       ]
     };
@@ -36,12 +54,26 @@ class Projects extends Component {
 
   render() {
     return (
-      <div className='container flex-col content-center'>
-        <h1 className='projects'>Check out past projects I've worked on solo and as a group!</h1>
-        <div className='projects flex-row'>
-          {this.state.projects.map(project => 
-            <ProjectItem imgURL={project.imgURL} color={project.color} title={project.title} />  
-          )}
+      <div className='container projects-container flex-row content-center'>
+        <div className='not-ongoing-container flex-col content-center'>
+          <h1 className='not-ongoing projects'>Check out past projects I've worked on solo and as a group!</h1>
+          <div className='not-ongoing projects flex-row'>
+            {this.state.projects.map(project => {
+              if (project.ongoing === false) {
+                return <ProjectItem key={project.id} imgURL={project.imgURL} color={project.color} title={project.title} />
+              }
+            })}
+          </div>
+        </div>
+        <div className='ongoing-container flex-col content-center'>
+          <h1 className='ongoing projects'>Here are some projects I'm currently working on with other developers...</h1>
+          <div className='ongoing projects flex-col'>
+            {this.state.projects.map(project => {
+              if (project.ongoing === true) {
+                return <ProjectItem key={project.id} imgURL={project.imgURL} color={project.color} title={project.title} />
+              }
+            })}
+          </div>
         </div>
       </div>
     );
--- src/components/components/ProjectItem.js
+++ src/components/components/ProjectItem.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
 
-const ProjectItem = (props) => (
-  <div className='project-item flex-row content-center'
-    style={{
-      backgroundImage: `url(${props.imgURL})`,
-      backgroundSize: 'cover'}}>
-    <div className='project-layer'
+const ProjectItem = props => (
+  <div className='flex-col content-center'>
+    <div className='project-item flex-row content-center'
       style={{
-        backgroundColor: `${props.color}`
-    }}></div>
-    <h1 className='project-title'>
-      {props.title}
-    </h1>
+        backgroundImage: `url(${props.imgURL})`,
+        backgroundSize: 'cover'}}>
+      <div className='project-layer'
+        style={{
+          backgroundColor: `${props.color}`
+      }}></div>
+      <h1 className='project-title'>
+        {props.title}
+      </h1>
+    </div>
+    <p className='project'>Hello</p>
   </div>
 );
 
