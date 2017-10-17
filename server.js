4ccf61c62e1a9de4a1f4c71472d7e5ec0f610113
Jieun Rim <rim.jieun@gmail.com>
2017-10-16 20:52:19 -0400

    testing /contact

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
 const bodyParser = require('body-parser');
-const routes = require('./routes/routes'); 
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use("/", routes);
 
-const db = process.env.MONGODB_URI || 'mongodb://localhost/projects';
+app.post('/submit', function(req, res) {
+  console.log('submit route: ', req.body);
+  res.redirect('/contact');
+});
 
-mongoose.Promise = bluebird;
 
-var promise = mongoose.connect(db, {
-  useMongoClient: true
-}).then( () => {
-  console.log('Mongoose connection is successful.');
-}, err => {
-  console.log(err);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, function() {
